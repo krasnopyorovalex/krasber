6a2fe5877b6b60f6e4b533a49ade44a8cc1a1dc5
Sanek <djShtaket88@mail.ru>
2019-03-11 17:43:17 +0300

    remove facebook pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,21 +151,6 @@
         gtag('config', 'UA-107358703-1');
     </script>
     <script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = 'https://vk.com/rtrg?p=VK-RTRG-224888-EpCh';</script>
-    <!-- Facebook Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '235500477021452');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" alt="facebook" style="display:none" src="https://www.facebook.com/tr?id=235500477021452&ev=PageView&noscript=1"/></noscript>
-    <!-- End Facebook Pixel Code -->
     <script src="{{ asset('js/app.min.js') }}"></script>
 </body>
 </html>
