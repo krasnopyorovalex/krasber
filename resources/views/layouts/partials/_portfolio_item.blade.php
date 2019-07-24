52d24e5867d352238d03dd0fd1302c5ea4b8daac
Sanek <djShtaket88@mail.ru>
2019-07-24 15:57:56 +0300

    update _portfolio_item.blade

--- resources/views/layouts/partials/_portfolio_item.blade.php
+++ resources/views/layouts/partials/_portfolio_item.blade.php
@@ -1,11 +1,13 @@
 <div class="portfolio__list-item {{ $portfolio->category }}">
     <div class="bg__item {{ $portfolio->color }}"></div>
     <div class="image__preview">
+        @if($portfolio->image)
         <figure>
             <a href="{{ $portfolio->url }}">
                 <img src="" data-src="{{ $portfolio->image->path }}" alt="{{ $portfolio->image->alt }}" title="{{ $portfolio->image->title }}">
             </a>
         </figure>
+        @endif
     </div>
     <div class="body">
         <div class="bg__box">
